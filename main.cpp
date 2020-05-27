a137c95c287c9e79e9c9b6197b19617232f62d99
Mario Lau <liuminghao233@gmail.com>
2020-05-27 21:31:26 +0800

    ..

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
-#include "Primitives/String.h"
 #include "Persistence/Restore.h"
 #include "Net/Server.h"
-
 int main() {
-
-    auto t1 = std::chrono::high_resolution_clock::now();
-//    String::SET(0, "key", "123");
-//    String::SET(0, "key2", "1234");
-//    String::SET(0, "key3", "12345");
-//
-//    Dump::BGSAVE();
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
     Restore::RESTORE();
     Server server;
     server.Run();
